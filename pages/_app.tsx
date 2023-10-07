1785d2cf581ccfa4b093368f43abdd9cdac8bd41
yokesh_ks <yokesh.ks@increscotech.com>
2023-10-07 16:52:35 +0530

    fix(meta): seo-meta added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,14 @@ import { ThemeProvider } from "@/components/common/providers";
 import { Inter } from "next/font/google";
 import { Header } from "@/components/common/Header";
 import { Toaster } from "@/components/ui/toaster";
+import { Meta } from "@/components/meta";
 
 const inter = Inter({ subsets: ["latin"] });
 
 function MyApp({ Component, pageProps }) {
   return (
     <div className={inter.className}>
+      <Meta />
       <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
         <Header />
         <Component {...pageProps} />
