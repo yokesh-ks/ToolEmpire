4ba7ab9b48f519d785a29b21efed93871dcbd908
yokesh_ks <yokesh.ks@increscotech.com>
2023-10-09 21:46:13 +0530

    fix(pwa): pwa setup

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -31,10 +31,25 @@ export default class MyDocument extends Document {
     return (
       <Html lang="en-GB">
         <Head>
+          <meta name="application-name" content="Tool Empire" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="default"
+          />
+          <meta name="apple-mobile-web-app-title" content="Tool Empire" />
+          <meta
+            name="description"
+            content="Innovative tools for empire builders."
+          />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="theme-color" content="#000000" />
           <link
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
             rel="stylesheet"
           />
+          <link rel="manifest" href="/manifest.json" />
           <link rel="icon" type="image/x-icon" href="/favicon.png"></link>
         </Head>
         <body>
