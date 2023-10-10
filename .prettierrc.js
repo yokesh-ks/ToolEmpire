644549778098e63a16e9eab210b174783a214264
yokesh_ks <yokesh.ks@increscotech.com>
2023-10-10 19:35:21 +0530

    feat(sort import): dev Dependency added

---
+++ .prettierrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  jsxSingleQuote: true,
+  plugins: [require.resolve('@trivago/prettier-plugin-sort-imports')],
+  importOrder: [
+    '^@/components/(.*)$',
+    '^@/constants/(.*)$',
+    '^@/container/(.*)$',
+    '^@/hooks/(.*)$',
+    '^@/icon/(.*)$',
+    '^@/store/(.*)$',
+    '^@/lib/(.*)$',
+    '^@/radix-ui/(.*)$',
+    '^@/utils/(.*)$',
+    '^@/(.*)$',
+    '^@/emails/(.*)$',
+    '^[./]',
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+}
